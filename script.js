68c699ecc535feba5630fa98e85a08ed7e7feb0e
Whit Will <147825238+Whit-Williams@users.noreply.github.com>
2023-12-12 18:45:21 +0000

    Edits to JS file, adding comments, tidying code

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Date for page here
+// Date and time for page here
 let today = dayjs();
 $("#currentDay").text(today.format("dddd DD MMMM YYYY"));
-// let projectTime = today.format("H:mm");
 let currentHour = parseInt(today.format("H"));
-console.log(currentHour);
 
-// Code for timeblocks go here
+// Timeblock hours text array
 let timeBlocks = [
   9,
   10,
@@ -16,27 +14,27 @@ let timeBlocks = [
   15,
   16,
   17,
-  22,
 ];
 
 let containerEl = $(".container");
 
+// Creates a new Bootstrap-powered HTML element for each array element
 timeBlocks.forEach(function (hour) {
   let timeblock = $("<div>");
   timeblock.addClass("row");
   containerEl.append(timeblock);
-
+// Timeblock hour text
   let hourSlot = $("<div>");
   hourSlot.addClass("col-1 hour");
   hourSlot.attr("id", "hour-" + hour);
   timeblock.append(hourSlot);
-
+// Timeblock text area
   let hourText = $('<textarea>');
   hourText.addClass("col-10 description");
   hourText.attr("name", "planner-text");
   hourText.attr("id", "text-" + hour);
   timeblock.append(hourText);
-
+// Timeblock save button
   let saveButton = $('<i class="fas fa-save"></i>')
   let saveColumn = $('<button>');
   saveColumn.addClass("col-1 saveBtn");
@@ -45,7 +43,6 @@ timeBlocks.forEach(function (hour) {
     let text = $(this).siblings(".description").val();
     localStorage.setItem(key,text);
   });
-  
   saveColumn.append(saveButton); 
   timeblock.append(saveColumn);
 });
